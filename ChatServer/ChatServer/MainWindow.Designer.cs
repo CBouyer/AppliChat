703e36047e491b64e9bb102a90f9d981e0a941b1
Evan Bedel <evan.bedel@lyceeastier.com>
2024-12-17 11:05:13 +0100

    Changement du nom de l'application (decision du CEO)

--- ChatServer/ChatServer/MainWindow.Designer.cs
+++ ChatServer/ChatServer/MainWindow.Designer.cs
@@ -28,107 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.buttonStop = new System.Windows.Forms.Button();
-            this.numericPort = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            this.buttonStart = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericPort)).BeginInit();
-            this.SuspendLayout();
+            groupBox1 = new GroupBox();
+            buttonStop = new Button();
+            numericPort = new NumericUpDown();
+            label1 = new Label();
+            buttonStart = new Button();
+            listBox1 = new ListBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericPort).BeginInit();
+            SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.buttonStop);
-            this.groupBox1.Controls.Add(this.numericPort);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.buttonStart);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(308, 58);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Configuration";
+            groupBox1.Controls.Add(buttonStop);
+            groupBox1.Controls.Add(numericPort);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(buttonStart);
+            groupBox1.Location = new Point(14, 14);
+            groupBox1.Margin = new Padding(4, 3, 4, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4, 3, 4, 3);
+            groupBox1.Size = new Size(359, 67);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Configuration";
             // 
             // buttonStop
             // 
-            this.buttonStop.Location = new System.Drawing.Point(219, 19);
-            this.buttonStop.Name = "buttonStop";
-            this.buttonStop.Size = new System.Drawing.Size(75, 23);
-            this.buttonStop.TabIndex = 4;
-            this.buttonStop.Text = "Stop";
-            this.buttonStop.UseVisualStyleBackColor = true;
-            this.buttonStop.Visible = false;
-            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            buttonStop.Location = new Point(255, 22);
+            buttonStop.Margin = new Padding(4, 3, 4, 3);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(88, 27);
+            buttonStop.TabIndex = 4;
+            buttonStop.Text = "Stop";
+            buttonStop.UseVisualStyleBackColor = true;
+            buttonStop.Visible = false;
+            buttonStop.Click += buttonStop_Click;
             // 
             // numericPort
             // 
-            this.numericPort.Location = new System.Drawing.Point(47, 22);
-            this.numericPort.Maximum = new decimal(new int[] {
-            65535,
-            0,
-            0,
-            0});
-            this.numericPort.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.numericPort.Name = "numericPort";
-            this.numericPort.Size = new System.Drawing.Size(78, 20);
-            this.numericPort.TabIndex = 3;
-            this.numericPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.numericPort.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
+            numericPort.Location = new Point(55, 25);
+            numericPort.Margin = new Padding(4, 3, 4, 3);
+            numericPort.Maximum = new decimal(new int[] { 65535, 0, 0, 0 });
+            numericPort.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericPort.Name = "numericPort";
+            numericPort.Size = new Size(91, 23);
+            numericPort.TabIndex = 3;
+            numericPort.TextAlign = HorizontalAlignment.Right;
+            numericPort.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Port : ";
+            label1.AutoSize = true;
+            label1.Location = new Point(7, 28);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Port : ";
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(219, 19);
-            this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(75, 23);
-            this.buttonStart.TabIndex = 0;
-            this.buttonStart.Text = "Start";
-            this.buttonStart.UseVisualStyleBackColor = true;
-            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            buttonStart.Location = new Point(255, 22);
+            buttonStart.Margin = new Padding(4, 3, 4, 3);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(88, 27);
+            buttonStart.TabIndex = 0;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
             // 
             // listBox1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 76);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(308, 108);
-            this.listBox1.TabIndex = 1;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(14, 88);
+            listBox1.Margin = new Padding(4, 3, 4, 3);
+            listBox1.Name = "listBox1";
+            listBox1.ScrollAlwaysVisible = true;
+            listBox1.Size = new Size(359, 124);
+            listBox1.TabIndex = 1;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 193);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.groupBox1);
-            this.MaximizeBox = false;
-            this.Name = "MainWindow";
-            this.Text = "Serveur de Tchatche";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericPort)).EndInit();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(404, 223);
+            Controls.Add(listBox1);
+            Controls.Add(groupBox1);
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            Name = "MainWindow";
+            Text = "VirtueServ";
+            FormClosing += MainWindow_FormClosing;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericPort).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
